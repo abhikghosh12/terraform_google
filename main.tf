3000f8b6d7a0b396f776d208fd52445e3a2de446
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-21 16:02:16 +0200

    please create

--- main.tf
+++ main.tf
@@ -28,3 +28,19 @@ module "voice_app" {
 
 
 data "google_client_config" "default" {}
+
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.cluster_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+  }
+
+}
--- provider.tf
+++ provider.tf
@@ -22,19 +22,6 @@ provider "google" {
   region  = var.region
 }
 
-# ... other code ...
 
-provider "kubernetes" {
-  host                   = "https://${module.gke.cluster_endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
-}
 
-provider "helm" {
-  kubernetes {
-    host                   = "https://${module.gke.cluster_endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
-  }
 
-}
