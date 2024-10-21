5920e540ed92b7ab3f8ed0782bd39059294e769a
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-21 16:11:17 +0200

    please create

--- main.tf
+++ main.tf
@@ -12,16 +12,19 @@ module "gke" {
 }
 
 module "voice_app" {
-  source       = "./modules/voice_app"
-  project_id   = var.project_id
-  region       = var.region
-  cluster_name = module.gke.cluster_name
-  namespace    = var.namespace
-  release_name = var.release_name
-  chart_path   = var.chart_path
-  webapp_image = var.webapp_image
-  worker_image = var.worker_image
-  domain_name  = var.domain_name
+  source                    = "./modules/voice_app"
+  project_id                = var.project_id
+  region                    = var.region
+  cluster_name              = module.gke.cluster_name
+  namespace                 = var.namespace
+  release_name              = var.release_name
+  chart_path                = var.chart_path
+  webapp_image              = var.webapp_image
+  worker_image              = var.worker_image
+  domain_name               = var.domain_name
+  cluster_endpoint          = module.gke.cluster_endpoint
+  cluster_ca_certificate    = module.gke.cluster_ca_certificate
+  google_client_access_token = data.google_client_config.default.access_token
 
   depends_on = [module.gke]
 }
