ed3e95fb45b3e32722db3c1868842b4a5bf4feba
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-21 15:33:44 +0200

    please create

--- main.tf
+++ main.tf
@@ -1,28 +1,4 @@
 # main.tf
-
-terraform {
-  required_version = ">= 0.14"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 module "gke" {
   source       = "./modules/gke"
   project_id   = var.project_id
@@ -30,20 +6,6 @@ module "gke" {
   cluster_name = var.cluster_name
   node_count   = var.node_count
   machine_type = var.machine_type
-}
-
-provider "kubernetes" {
-  host                   = "https://${module.gke.cluster_endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = "https://${module.gke.cluster_endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
-  }
 }
 
 module "voice_app" {
---
+++ provider.tf
@@ -0,0 +1,45 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# ... other code ...
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+
+  # Ensure the provider is configured AFTER the GKE cluster is ready
+  depends_on = [module.gke] 
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.cluster_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+  }
+
+  # Ensure the provider is configured AFTER the GKE cluster is ready
+  depends_on = [module.gke] 
+}
