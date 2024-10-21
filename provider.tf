84f0094a1aa932f2f60d8693b87f7ec04ea1d938
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-21 16:46:22 +0200

    please create

--- provider.tf
+++ provider.tf
@@ -28,26 +28,30 @@ provider "google" {
   region  = var.region
 }
 
+data "google_client_config" "default" {}
+
 data "google_container_cluster" "my_cluster" {
   name     = var.cluster_name
   location = var.region
-  project  = var.project_id
 }
 
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate
+  )
 }
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+    host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate
+    )
   }
 }
-
 
 
 
